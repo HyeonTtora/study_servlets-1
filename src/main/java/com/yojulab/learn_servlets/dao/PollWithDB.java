5784035430c76a3dc311698b9e089e024283b43c
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2022-12-21 18:26:48 +0900

    1221

---
+++ src/main/java/com/yojulab/learn_servlets/dao/PollWithDB.java
@@ -0,0 +1,30 @@
+package com.yojulab.learn_servlets.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+public class PollWithDB {
+    public void getQuestion(String questionsUid) throws SQLException {
+        // SELECT * FROM QUESTIONS_LIST
+        // WHERE QUESTIONS_UID = 'Q2';
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+
+        String query = "SELECT * FROM QUESTIONS_LIST" + "WHERE QUESTIONS_UID = '" + questionsUid + "'";
+
+
+        
+        ResultSet resultSet = statement.executeQuery(query);
+        HashMap<String, Object> result;
+        while (resultSet.next()) {
+            resultSet.getString("QUESTIONS_UID");
+            resultSet.getString("QUESTIONS");
+            resultSet.getInt("ORDERS");
+        }
+
+
+    }
+
+}
