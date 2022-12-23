c2abc5f1f00d9c81aa8858778cbdc60d9098bf37
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2022-12-23 15:17:34 +0900

    1223

---
+++ src/main/java/com/yojulab/learn_servlets/servlets/CreateSessionJSPServlets.java
@@ -0,0 +1,71 @@
+package com.yojulab.learn_servlets.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+// /session/createJSPServlets?username=htora&password=1234
+@WebServlet(urlPatterns = "/session/createJSPServlets")
+public class CreateSessionJSPServlets extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // display
+
+        // login
+        HttpSession httpSession = null;
+        HttpSession httpSession_false = null;
+        // // EXISTS JESSIONID
+        // httpSession_false = request.getSession(false); // 존재하면 인스턴스화
+        // httpSession = request.getSession();// 무족언 인스턴스화
+
+        // NOT EXISTS JESSIONID
+        // httpSession_false = request.getSession(false); //Null
+        // httpSession = request.getSession(); // 무조건 인스턴스화
+
+        String path = null;
+        if ("htora".equals(username) && "1234".equals(password)) {
+            // login
+            httpSession_false = request.getSession(false); // 존재하면 인스턴스화
+
+            if (httpSession == null) {
+                httpSession = request.getSession();// 무족언 인스턴스화
+                httpSession.setAttribute("username", username);
+                httpSession.setAttribute("password", password);
+            }
+
+            System.out.println(httpSession.getAttribute("username"));
+            System.out.println(httpSession.getId());
+
+            path = "/session/checkLogin.jsp";
+
+        } else {
+            // logout
+            httpSession = request.getSession(false); // 존재하면 인스턴스화
+            if (httpSession != null) {
+                httpSession.invalidate();
+
+            }
+            // 에러남
+            // System.out.println(httpSession.getAttribute("username"));
+            // System.out.println(httpSession.getId());
+            path = "/session/checkLogout.jsp";
+        }
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+        requestDispatcher.forward(request, response);
+    }
+
+}
